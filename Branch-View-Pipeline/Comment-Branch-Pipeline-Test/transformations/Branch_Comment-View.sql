4a99a3b64bc37dd0c2da812bc4182e91615b8b08
TarunJain81 <ctarunjain81@gmail.com>
2026-02-03 10:42:09 +0000

    Branch View Sql File

---
+++ Branch-View-Pipeline/Comment-Branch-Pipeline-Test/transformations/Branch_Comment-View.sql
@@ -0,0 +1,16 @@
+
+CREATE OR REPLACE VIEW `catalog-test`.`schema-test`.nyctaxi_daily_summary
+(
+  trip_date        COMMENT 'Date of the trip (pickup date)',
+  trip_count       COMMENT 'Total trips for that day and passenger count',
+  avg_total_amount COMMENT 'Average total fare amount for those trips'
+)
+COMMENT 'Daily NYC taxi summary by passenger count'
+AS
+SELECT
+  DATE(tpep_pickup_datetime) AS trip_date,
+  COUNT(*)                   AS trip_count,
+  AVG(fare_amount)          AS avg_total_amount
+FROM samples.nyctaxi.trips
+GROUP BY
+  DATE(tpep_pickup_datetime)
