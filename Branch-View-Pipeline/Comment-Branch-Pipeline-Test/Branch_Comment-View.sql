2882d4b3f98885cc2d35cdebc681d82f1416475d
TarunJain81 <ctarunjain81@gmail.com>
2026-02-04 11:13:29 +0000

    Update column type

--- Branch-View-Pipeline/Comment-Branch-Pipeline-Test/Branch_Comment-View.sql
+++ Branch-View-Pipeline/Comment-Branch-Pipeline-Test/Branch_Comment-View.sql
@@ -1,9 +1,9 @@
 
 CREATE OR REPLACE VIEW nyctaxi_daily_summary_branch
 (
-  trip_date        COMMENT 'Date of the trip (pickup date)',
-  trip_count       COMMENT 'Total trips for that day and passenger count',
-  avg_total_amount COMMENT 'Average total fare amount for those trips'
+  trip_date    DATE    COMMENT 'Date of the trip (pickup date)',
+  trip_count    BIGINT   COMMENT 'Total trips for that day and passenger count',
+  avg_total_amount DOUBLE COMMENT 'Average total fare amount for those trips'
 )
 COMMENT 'Daily NYC taxi summary by passenger count'
 AS
